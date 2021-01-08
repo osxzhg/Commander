11c0b104d68fb311eba435a1fddf4b61c7e90320
osxzhg <osxzhg@gmail.com>
2021-01-08 22:43:39 +1300

    add annotation to createDto

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(250)]
         public string Howto { get; set; }
         public string Line { get; set; }
         public string Platform { get; set; }
